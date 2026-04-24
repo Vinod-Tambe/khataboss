36478ec3d2c6dd4f9c3afbb89b1cc3d13d9e47a9
gauri mahakal <gaurimahakal24@gmail.com>
2026-04-24 22:03:13 +0530

    ui changes

--- src/components/finance/Finance.jsx
+++ src/components/finance/Finance.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import FinanceInfo from './FinanceInfo'
-import FinanceHistory from './FinanceHistory'
-import PaymentForm from './PaymentForm'
+    import React from 'react'
+    import FinanceInfo from './FinanceInfo'
+    import FinanceHistory from './FinanceHistory'
+    import PaymentForm from './PaymentForm'
 
-const Finance = () => {
-    return (
-        <>
-            <div className="row g-3 ">
-                <PaymentForm />
-                <div className="col-md-6 border shadow-sm">
-                    <h5 className="text-muted pt-1">Finance Information</h5>
-                    <FinanceInfo />
+    const Finance = () => {
+        return (
+            <>
+                <div className="row g-3 ">
+                    <div className="col-md-12">
+                        <PaymentForm />
+                    </div>
+                    
+                    <div className="col-md-12 py-3 px-3 rounded border shadow-sm">
+                        <h5 className="text-muted pt-1">Finance Information</h5>
+                        <FinanceInfo />
+                    </div>
+                    <div className="col-md-12">
+                        <FinanceHistory />
+                    </div>
                 </div>
-                <div className="col-md-12">
-                    <FinanceHistory />
-                </div>
-            </div>
-        </>
-    )
-}
+            </>
+        )
+    }
 
-export default Finance
+    export default Finance
