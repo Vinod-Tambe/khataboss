ac64eebd5c4a092e26094aa702a8c1d5da9db9e2
Vinod Tambe <vtambe@novatechset.com>
2026-09-16 16:30:36 +0530

    Resolved Issue

--- src/pages/support/SupportTicketDetailPage.jsx
+++ src/pages/support/SupportTicketDetailPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 /** Deep links to /support/:uuid open the list drawer. */
